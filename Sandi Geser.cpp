1c5c79ba6f6b1c7e2350a429009ef03160451f13
O. Riastanjung <87843869+oriastanjung@users.noreply.github.com>
2022-09-29 23:30:08 +0700

    Update Sandi Geser.cpp

--- Sandi Geser.cpp
+++ Sandi Geser.cpp
@@ -15,24 +15,32 @@ void solve(string input, int key)
         {
             continue;
         }
-        else if (int(input[i]) >= 41 && int(input[i]) <= 90)
+        else if (input[i] >= 'A' && input[i] <= 'Z')
         {
             temp = int(input[i]);
             if (temp + key > 90)
             {
-                input[i] = char(key - (90 - temp) + 40);
+                input[i] = char(key - (90 - temp) + 64);
+            }
+            else if (temp + key < 65)
+            {
+                input[i] = char(91 + (key - (65 - temp)));
             }
             else
             {
                 input[i] = char(temp + key);
             }
         }
-        else if (int(input[i]) >= 97 && int(input[i]) <= 122)
+        else if (input[i] >= 'a' && input[i] <= 'z')
         {
             temp = int(input[i]);
             if (temp + key > 122)
             {
                 input[i] = char(key - (122 - temp) + 96);
+            }
+            else if (temp + key < 97)
+            {
+                input[i] = char(123 + (key - (97 - temp)));
             }
             else
             {
